f06e7313f7449c7f447b74d2b216291e8d33521b
Clayton Grassick <clayton@claytronics.org>
2021-01-14 10:07:45 -0500

    Basic page structure

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # PAVICS-landing
 The landing page serving as the entrance to PAVICS
+
+
+# Serving content
+
+Run `npx http-server src -c-1` to view, or use any other webserver to serve the contents of `src` folder.
---
+++ src/assets/setup.js
@@ -0,0 +1,20 @@
+/* Set up the template including the navbar */
+
+$(function() {
+  // Load template
+  $("body").load("assets/template.html", function() {
+    // Get current page url
+    const page = window.location.pathname.split("/")[window.location.pathname.split("/").length - 1]
+
+    // Highlight nav-link
+    $("li.nav-item").each(function() {
+      // If current page, add active
+      if ($(this).find("a").attr("href") == page) {
+        $(this).addClass("active")
+      }
+    })
+
+    // Add content
+    $("#main").load("pages/" + page)
+  })
+})
