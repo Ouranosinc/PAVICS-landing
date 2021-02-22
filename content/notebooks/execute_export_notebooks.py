e0229107be88132f70c12e7521f96754223ee7cf
tlogan2000 <logan.travis@ouranos.ca>
2021-02-22 17:23:34 -0500

    * html versions update

--- content/notebooks/execute_export_notebooks.py
+++ content/notebooks/execute_export_notebooks.py
@@ -1,7 +1,7 @@
 import subprocess
 from pathlib import Path
-for nb in [p  for p in Path('.').rglob('*.ipynb') if 'checkpoints' not in p.as_posix()]:
-    cmd = f"jupyter nbconvert --ExecutePreprocessor.timeout=600 --to html --execute {nb.as_posix()}"
+for nb in [p  for p in Path('.').rglob('*5Vis*.ipynb') if 'checkpoints' not in p.as_posix()]:
+    cmd = f"jupyter nbconvert --ExecutePreprocessor.timeout=6000 --to html --execute {nb.as_posix()}"
     try:
         subprocess.run(cmd, shell=True, check=True)
     except:
