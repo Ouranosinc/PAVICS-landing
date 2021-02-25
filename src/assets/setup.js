ee018860ea20f97c94d40986f1e0cf15011f7a59
Clayton Grassick <clayton@claytronics.org>
2021-02-25 10:47:04 -0500

    Fix iFrame scroll issue

--- src/assets/setup.js
+++ src/assets/setup.js
@@ -1,27 +1,6 @@
 /* Set up the template including the navbar */
 
 
-/* Autosizes an iframe */
-function autosizeIframes() {
-  // Delay to ensure fully loaded
-  setTimeout(function() {
-    $("iframe").each(function() {
-      // Can't resize if invisible
-      if (!$(this).is(":visible")) {
-        return
-      }
-      if (this.contentWindow.document.body) {
-        this.style.height = (this.contentWindow.document.body.scrollHeight + 20) + 'px';
-      }
-      else {
-        this.onload = () => {
-          this.style.height = (this.contentWindow.document.body.scrollHeight + 20) + 'px';
-        } 
-      }
-    })
-  }, 100)
-}
-
 $(function() {
   // Load template
   $("body").load("assets/template.html", function() {
@@ -37,13 +16,7 @@ $(function() {
     })
 
     // Add content
-    $("#main").load("pages/" + page, function() {
-      // Autosize iframes and redo whenever tab is clicked (as invisible ones can't be sized)
-      autosizeIframes()
-      $("a.nav-link").on("click", function() {
-        autosizeIframes()
-      })
-    })
+    $("#main").load("pages/" + page)
   })
 })
 
