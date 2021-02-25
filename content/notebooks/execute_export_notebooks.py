a45c6a0cc9189faaf3f9654686ad80b64da02bbb
tlogan2000 <logan.travis@ouranos.ca>
2021-02-25 16:55:27 -0500

    * fix hyperlinks for iframe

--- content/notebooks/execute_export_notebooks.py
+++ content/notebooks/execute_export_notebooks.py
@@ -1,8 +1,18 @@
 import subprocess
 from pathlib import Path
-for nb in [p  for p in Path('.').rglob('*5Vis*.ipynb') if 'checkpoints' not in p.as_posix()]:
-    cmd = f"jupyter nbconvert --ExecutePreprocessor.timeout=6000 --to html --execute {nb.as_posix()}"
+import shutil
+import git
+repo = git.Repo('.', search_parent_directories=True)
+repo = Path(repo.git_dir).parent
+run_flag = False
+for nb in [p  for p in Path('.').rglob('*.ipynb') if 'checkpoints' not in p.as_posix()]:
+    if run_flag:
+        cmd = f"jupyter nbconvert --ExecutePreprocessor.timeout=6000 --to html --execute {nb.as_posix()}"
+    else:
+        cmd = f"jupyter nbconvert --to html {nb.as_posix()}"
     try:
         subprocess.run(cmd, shell=True, check=True)
+        htmlfile = nb.parent.joinpath(nb.name.replace('.ipynb','.html'))
+        shutil.copyfile(htmlfile.as_posix(), repo.joinpath('src/assets/notebooks/').joinpath(htmlfile.name).as_posix())
     except:
         print(nb)
