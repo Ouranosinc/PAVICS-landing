8c16df565cc0b258cc251e93f94df9a308c61687
tlogan2000 <logan.travis@ouranos.ca>
2026-05-22 09:46:01 -0400

    custom sort of json

--- content/notebooks/datasets/generate_dataset_manifest.py
+++ content/notebooks/datasets/generate_dataset_manifest.py
@@ -16,13 +16,14 @@ for category in sorted([d for d in ROOT.iterdir() if d.name.startswith('Datasets
 
     all_subdirs = sorted([d for d in category.iterdir() if d.is_dir()])
     subdirs = [d for d in all_subdirs if 'Ouranos' in d.name ]
+    subdirs.extend([d for d in all_subdirs if 'Environment and Climate Change Canada' in d.name ])
     subdirs.extend([d for d in all_subdirs if d not in subdirs])
     for dirpath in subdirs:
         filenames = [f.name for f in dirpath.iterdir() if f.is_file()]
         files = sorted([f for f in filenames if f.endswith('.html')])
-        if 'Ouranos' in dirpath.name:
-            sort_order = ['CRCM5-CMIP6', 'ESPO-G6-R2 v1.0.0 _ Ouranos', 'ESPO-G6-R2 v1.0.0 _ Derived', 'ESPO-G6-E5L', 'PINS', 'ClimEX']
-            files.sort(key=lambda x: sort_order.index(next((s for s in sort_order if s in x), '')) if any(s in x for s in sort_order) else len(sort_order))
+        
+        sort_order = ['CRCM5-CMIP6', 'ESPO-G6-R2 v1.0.0 _ Ouranos', 'ESPO-G6-R2 v1.0.0 _ Derived', 'ESPO-G6-E5L', 'PINS', 'ClimEX', 'CanDCS-M6']
+        files.sort(key=lambda x: sort_order.index(next((s for s in sort_order if s in x), '')) if any(s in x for s in sort_order) else len(sort_order))
 
         if files:
             subfolders[dirpath.name] = files
